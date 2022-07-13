8a3dcceba772d715051fe8d26b69f6b152c2932b
Mike van Eckendonk <mike.e@vecks.com.br>
2022-07-13 05:19:13 -0300

    Fixed eslint Missing semicolon

--- tests/reverse.test.js
+++ tests/reverse.test.js
@@ -1,13 +1,13 @@
 const reverseString = require('../functions/reverse');
 
 test('Expect that this test reverses string', () => {
-  const str = 'BRAZIL' // Arrange
-  const ans = reverseString(str) // Act
+  const str = 'BRAZIL'; // Arrange
+  const ans = reverseString(str); // Act
   expect(ans).toBe('LIZARB'); // Assert
 });
 
 test('reversed "test" to "tset"', () => {
   const str = 'test'; // Arrange
-  const ans = reverseString(str) // Act
+  const ans = reverseString(str); // Act
   expect(ans).toBe('tset'); // Assert
 });
